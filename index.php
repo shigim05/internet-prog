b26c7772c8d660d5a533af3d56bf1f5ff008b7c3
Shigim <57025496+shigim05@users.noreply.github.com>
2020-12-10 23:16:16 +0500

    add index

---
+++ index.php
@@ -0,0 +1,16 @@
+﻿<p> Дата и время:
+<p>
+<?php
+
+	$d=date("d.m.Y H:i");
+	echo($d);
+echo '<br>';
+echo 'Мирзаева Ш.А. <br>';
+?>
+<ul>
+    <li><a href="http://f0474103.xsph.ru/1lab.php">ЛР №1</a></li>
+	<li><a href="http://f0474103.xsph.ru/2lab.php">ЛР №2</a></li>
+	<li><a href="http://f0474103.xsph.ru/3lab.php">ЛР №3</a></li>
+	<li><a href="http://f0474103.xsph.ru/bd_naselennyye_punkty/index.php">ЛР №4</a></li>
+	<li><a href="http://f0474103.xsph.ru/city5/index.php">ЛР №5</a></li>
+</ul>
